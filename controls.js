ab34a777fb9198f4a6b7e1540875e654e6925ca5
Thibaut Izard <thibaut.izard@gmail.com>
2020-11-24 21:59:47 +0100

    Initial commit

---
+++ controls.js
@@ -0,0 +1,98 @@
+const button = document.querySelector(".submit-button")
+
+const testInput = input => {
+
+    switch(input.id) {
+        case "email" :
+            if(/[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/.test(input.value)) {input.classList.remove("invalid"); input.classList.add("valid")}
+            else {input.classList.remove("valid"); input.classList.add("invalid")}
+            break;
+
+        case "email-confirmation" :
+            if(input.value.length > 0 && /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}/.test(input.value)) {input.classList.remove("invalid"); input.classList.add("valid")}
+            else {input.classList.remove("valid"); input.classList.add("invalid")}
+            break;
+        
+        case "telephone" : 
+            if(/^0[0-9]{9}$/.test(input.value.replace(/\s/g,''))) {input.classList.remove("invalid"); input.classList.add("valid")}
+            else {input.classList.remove("valid"); input.classList.add("invalid")}
+            break;
+        
+        case "informations-enquete" : 
+            if(input.value.replace(/\s/g,'').length > 3) {input.classList.remove("invalid"); input.classList.add("valid")}
+            else {input.classList.remove("valid"); input.classList.add("invalid")}
+            break;
+        
+        case "informations-identifiant" : 
+            if(input.value.replace(/\s/g,'').length > 3) {input.classList.remove("invalid"); input.classList.add("valid")}
+            else {input.classList.remove("valid"); input.classList.add("invalid")}
+            break;
+
+        case "dysfonctionnement" : 
+            if(input.value.startsWith("https://www.insee.fr/")) {input.classList.remove("invalid"); input.classList.add("valid")}
+            else {input.classList.remove("valid"); input.classList.add("invalid")}
+            break;
+    }
+
+    checkAll()
+
+}
+
+const checkAll = () => {
+    const fieldsToCheck = Array.from(document.querySelectorAll(".form-group"))
+                               .filter(field => getComputedStyle(field).display !== "none" && !field.classList.contains("optional"))
+                               .map(field => field.querySelector(".box-value, input, textarea"))
+    if(fieldsToCheck.every(field => field.classList.contains("valid"))) button.classList.add("active")
+    else button.classList.remove("active")
+}
+
+
+button.addEventListener("click", () => {
+
+    if(button.classList.contains("active")) {
+
+        const fieldsToCheck = Array.from(document.querySelectorAll(".form-group"))
+                               .filter(field => getComputedStyle(field).display !== "none" && !field.classList.contains("optional"))
+                               .map(field => field.querySelector(".box-value, input, textarea"))
+        
+        const inputsToEmpty = Array.from(document.querySelectorAll("input"))
+        const boxValuesToEmpty = Array.from(document.querySelectorAll(".box-value"))
+        const textAreaToEmpty = Array.from(document.querySelectorAll("textarea"))
+
+        inputsToEmpty.forEach(input => {
+            input.value=""
+            input.classList.remove("valid")
+            input.classList.remove("invalid")
+        })
+        boxValuesToEmpty.forEach(boxValue => {
+            boxValue.querySelector("span").textContent="Choisissez"
+            boxValue.classList.remove("valid")
+            boxValue.classList.remove("invalid")
+        })
+        textAreaToEmpty.forEach(textArea => {
+            textArea.value=""
+            textArea.classList.remove("valid")
+            textArea.classList.remove("invalid")
+            textArea.parentNode.querySelector(".remaining").textContent = "0 caractère sur 2 000 caractères autorisés"
+            console.log(textArea.parentNode.querySelector(".remaning"))
+        })
+
+        alert("Votre demande a bien été envoyée ! Vous recevrez une réponse à votre demande d'information dans un délai de quelques jours ouvrés.")
+    }
+
+    else {
+        const fieldsToHighlight = Array.from(document.querySelectorAll(".form-group"))
+                                  .filter(field => !field.classList.contains("optional"))
+
+
+        alert("Merci de remplir tous les champs obligatoires")
+        
+    }
+
+    checkAll()
+})
+
+export {
+    checkAll,
+    testInput
+}
---
+++ script.js
@@ -0,0 +1,123 @@
+import { testInput, checkAll } from './controls.js'
+
+
+const formGroups = document.querySelectorAll(".form-group") 
+const boxValues =  document.querySelectorAll(".box-value")
+const optionals = document.querySelectorAll(".optional")
+
+const selects = document.querySelectorAll(".custom-select")
+const textarea = document.querySelector("textarea")
+const inputs = document.querySelectorAll("input")
+
+const button = document.querySelector(".submit-button")
+
+
+// SELECTION MENUS
+
+selects.forEach(select => {
+
+    const modalities = select.querySelectorAll("li")
+    const boxValue = select.querySelector(".box-value")
+    const boxValueMessage = select.querySelector(".box-value span")
+    
+    modalities.forEach(modality => modality.addEventListener("click", () => {
+
+        // Replace text by modality selected
+        boxValueMessage.textContent = modality.textContent
+        boxValue.classList.remove("expand")
+
+        // Switch current modality to selected
+        modalities.forEach(modality => modality.classList.remove("selected"))
+        modality.classList.add("selected")
+
+        boxValue.classList.add("valid")
+
+        displayFields(select, modality)
+
+        checkAll()
+
+        
+    }))
+})
+
+// INPUTS
+
+inputs.forEach(input => {
+
+    input.addEventListener("input", () => {
+
+        testInput(input)
+
+        if(input.classList.contains("email")) {
+
+            const emails = Array.from(inputs).filter(input => input.classList.contains("email"))
+
+            if(emails[0].value !== emails[1].value && emails[0].value && emails[1].value) {
+                emails[1].classList.remove("valid")
+                emails[1].classList.add("invalid")
+            } else if(emails[0].value === emails[1].value ) {
+                emails[1].classList.remove("invalid")
+                emails[1].classList.add("valid")
+            }
+        }
+
+        checkAll()
+    })
+
+    input.addEventListener("change", () => testInput(input))
+
+})
+
+// TEXT AREA
+
+textarea.addEventListener("input", e => {
+    const remaining = e.target.parentNode.querySelector(".remaining")
+    const newCount = e.target.value.length
+    remaining.textContent = `${newCount} caractère${Math.abs(newCount) === 1 || newCount === 0 ? "" : "s"} sur 2 000 caractères autorisés`
+    if(newCount >= 2000) remaining.style.color = "red"
+    else remaining.style.color = "black"
+
+    if(newCount > 0) e.target.classList.add("valid")
+
+    checkAll()
+})
+
+textarea.addEventListener("change", e => checkAll())
+
+
+// BOX VALUES
+
+boxValues.forEach(boxValue => boxValue.addEventListener("click", _ => boxValue.classList.toggle('expand')))
+
+
+
+// FUNCTIONS
+
+const displayFields = (select, modality) => {
+
+    if(select.id === "categorie") {
+        switch(modality.textContent) {
+            case "Obtenir des informations sur une enquête en cours" :
+            Array.from(formGroups).filter(formGroup => formGroup.classList.contains("form-group--informations"))
+                                  .forEach(formGroup => formGroup.style.display = "flex")
+            
+            Array.from(formGroups).filter(formGroup => formGroup.classList.contains("form-group--dysfonctionnement"))
+                                  .forEach(formGroup => formGroup.style.display = "none")
+            
+            return
+
+            case "Signaler un dysfonctionnement" : 
+            Array.from(formGroups).filter(formGroup => formGroup.classList.contains("form-group--dysfonctionnement"))
+                                  .forEach(formGroup => formGroup.style.display = "flex")
+
+            Array.from(formGroups).filter(formGroup => formGroup.classList.contains("form-group--informations"))
+                                  .forEach(formGroup => formGroup.style.display = "none")
+            return
+
+            default :
+            Array.from(formGroups).filter(formGroup => formGroup.classList.contains("subfilter"))
+                                  .forEach(formGroup => formGroup.style.display = "none") 
+
+        }
+    }
+}
